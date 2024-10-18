88aab0feac0fe13bbbfb439f8af800effd4957a0
ether-Drifter <ether.drifter911@protonmail.com>
2024-10-18 21:10:45 +0530

    Initialized Swift project

---
+++ qQuick/qQuick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  qQuick
+//
+//  Created by Hani Atta on 18/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
