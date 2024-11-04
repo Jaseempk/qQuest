60224427aeafb9652f6be44a12f4d32e71c4c41c
Jaseem <jasimpk011@gmail.com>
2024-11-04 06:19:21 +0530

    fix:deployment errors fixed

--- qquestdapp/ConnectKit/Web3Provider.tsx
+++ qquestdapp/ConnectKit/Web3Provider.tsx
@@ -2,29 +2,30 @@ import { WagmiProvider, createConfig, http } from "wagmi";
 import { mainnet, base, baseSepolia } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { ReactNode } from "react"; // Add this import
 
 export const config = createConfig(
   getDefaultConfig({
-    // Your dApps chains
     chains: [baseSepolia],
     transports: {
-      // RPC URL for each chain
       [baseSepolia.id]: http(
         "https://base-sepolia.g.alchemy.com/v2/txntl9XYKWyIkkmj1p0JcecUKxqt9327"
       ),
     },
-
-    // Required API Keys
     walletConnectProjectId: "b1647c589ac18a28722c490d2f840895",
-
-    // Required App Info
     appName: "QQuest",
   })
 );
 
 const queryClient = new QueryClient();
 
-export const Web3Provider = ({ children }) => {
+// Add type for the props
+interface Web3ProviderProps {
+  children: ReactNode;
+}
+
+// Use the interface in the component definition
+export const Web3Provider = ({ children }: Web3ProviderProps) => {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
